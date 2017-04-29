e1913199b4b3e3bb86f59727331c7d9aa0818bf9
stefan-matcovici <stefan.matcovici@yahoo.com>
2017-04-29 11:24:52 +0300

    Basic components
    Generated events, event-detail, dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EventsComponent } from './events/events.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    EventsComponent,
+    EventDetailComponent
   ],
   imports: [
     BrowserModule,
