2d57b997e6fec8631e3231a9055183c78616d0e9
stefan-matcovici <stefan.matcovici@yahoo.com>
2017-05-08 22:12:51 +0300

    updated test once more

--- src/app/services/event.service.spec.ts
+++ src/app/services/event.service.spec.ts
@@ -1,28 +1,50 @@
-import {Injectable, ReflectiveInjector} from '@angular/core';
-import {async, fakeAsync, tick} from '@angular/core/testing';
-import {BaseRequestOptions, ConnectionBackend, Http, RequestOptions} from '@angular/http';
-import {Response, ResponseOptions} from '@angular/http';
-import {MockBackend, MockConnection} from '@angular/http/testing';
+import { inject, fakeAsync, tick, TestBed } from '@angular/core/testing'
+import { MockBackend } from '@angular/http/testing'
+import { Http, ConnectionBackend, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
 
 import { Event } from '../models/Event';
-
 import { EventService } from './event.service';
-
-const EVENT_ONE = 'EventNrOne';
-const EVENT_TWO = 'WillBeAlwaysTheSecond';
 
 describe('EventService', () => {
   beforeEach(() => {
-    this.injector = ReflectiveInjector.resolveAndCreate([
-      { provide: ConnectionBackend, useClass: MockBackend },
-      { provide: RequestOptions, useClass: BaseRequestOptions },
-      Http,
-      EventService,
-    ]);
-    this.eventService = this.injector.get(EventService);
-    this.backend = this.injector.get(ConnectionBackend) as MockBackend;
-    this.backend.connections.subscribe((connection: any) => this.lastConnection = connection);
+    TestBed.configureTestingModule({
+      providers: [
+        BaseRequestOptions,
+        MockBackend,
+        EventService,
+        {
+          provide: Http,
+          useFactory: (backend: ConnectionBackend,
+            defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          }, deps: [MockBackend, BaseRequestOptions]
+        },
+      ]
+    })
   });
+
+  it('retrieves using the event ID',
+    inject([EventService, MockBackend], fakeAsync((eventService, mockBackend
+    ) => {
+      var res;
+      mockBackend.connections.subscribe(c => {
+        expect(c.request.url).toBe('api/events');
+        let response = new ResponseOptions({ body: JSON.stringify({data: [{id:1}, {id:2}]}) });
+        c.mockRespond(new Response(response));
+      });
+      eventService.getEvents().then((events : Event[]) => {
+        res = events;
+      });
+      tick();
+      console.log(res);
+      expect(res[0].id).toEqual(1);
+    }))
+  );
+});
+
+describe('getEvent', () => {
+  
+});
 
   //it('should ...', inject([EventService], (service: EventService) => {
   //  expect(service).toBeTruthy();
@@ -31,23 +53,3 @@ describe('EventService', () => {
   //it('Deleting a non-existing record should throw an error', inject([EventService], (service: EventService) => {
   //  expect(service.delete(-1)).toThrowError;
   //}));
-
-  it('getEvents() should query current service url', () => {
-    console.log(this.eventService.getEvents());
-    expect(this.lastConnection).toBeDefined('no http service connection at all?');
-    expect(this.lastConnection.request.url).toMatch(/api\/events$/, 'url invalid');
-  });
-
-  it('getEvents() should return some events', fakeAsync(() => {
-       let result: String[];
-       this.eventService.getEvents().then((events: String[]) => result = events);
-       this.lastConnection.mockRespond(new Response(new ResponseOptions({
-         body: JSON.stringify({data: [EVENT_ONE, EVENT_TWO]}),
-       })));
-       
-       tick();
-       expect(result.length).toEqual(2, 'should contain given amount of events');
-       expect(result[0]).toEqual(EVENT_ONE, ' EVENT_ONE should be the first hero');
-       expect(result[1]).toEqual(EVENT_TWO, ' EVENT_TWO should be the second hero');
-     }));
-});
